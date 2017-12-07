2a02eb1a4d04935d36d833872ce8d7cb8735149f
Remi Kristelijn <rkristelijn@gmail.com>
2017-12-07 09:46:46 +0000

    adhering to name convention of private properties

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,10 +6,10 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private albumUrl: string = '../assets/album.json';
-  constructor(private http: Http) { }
+  private _albumUrl: string = '../assets/album.json';
+  constructor(private _http: Http) { }
   getAlbum(id : number): Observable<any> {
-    return this.http.get(this.albumUrl)
+    return this._http.get(this._albumUrl)
       .map(response => response.json())
   }
 }
