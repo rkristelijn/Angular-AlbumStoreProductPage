f5b5d6aae7110b6e8bdca6de43fdc73e1fd13840
Remi Kristelijn <rkristelijn@gmail.com>
2017-12-13 10:19:06 +0000

    https://github.com/rkristelijn/Angular-AlbumStoreProductPage/blob/38562500f1275d7c05244752708880a253987287/src/spec/mocha-specs/part4/product-service-types-getalbum-responsejson-to-album.mocha.spec.js looked at test, decided to remove comments

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,6 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
-//import 'rxjs/add/operator/delay';
 
 import { Album } from './album';
 
@@ -12,26 +11,8 @@ export class ProductService {
   private _albumUrl: string = '../assets/album.json';
   constructor(private _http: Http) { }
   public getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl)
-      //.delay(5000)
-      .map(
-        // (response) => {
-        //   //http://acdcjunior.github.io/typescript-cast-object-to-other-type-or-instanceof.html
-        //   //https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-guards-and-differentiating-types
-        //   //console.log(response, <Album>response.json(), typeof(<Album>response.json()));
-        //   // return <Album>response.json(); // The getAlbum method isn't returning the correct response.
-        //   let ret : Album;
-        //   ret = <Album>response.json().album; 
-        //   //console.log(ret, 'instance of Album?');
-        //   //if(this.isAlbumTypeGuard(ret)) console.log('yes'); else console.log('no');
-        //   return ret;
-        // }
-        response => response.json() as Album
+    return this._http.get(this._albumUrl).map(
+        response => <Album>response.json()
       )
-  }
-
-  isAlbumTypeGuard(album: any): album is Album {
-    console.log('isAlbumTypeGuard', album.name);
-    return album.name !== undefined && album.releaseDate!==undefined;
   }
 }
